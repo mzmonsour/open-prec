e4668078983d2cdc98c2ce94e543977ba6c0d2ed
Matt Monsour <mzmonsour@gmail.com>
2015-07-12 18:39:27 -0400

    Implement print macros and colored printing

---
+++ src/colordef.h
@@ -0,0 +1,10 @@
+
+#ifndef OPENPREC_COLORDEF_H
+#define OPENPREC_COLORDEF_H
+
+#include <Color.h>
+
+const static Color DEEP_ORANGE(173, 81, 21, 255);
+const static Color ORANGE(207, 106, 50, 255);
+
+#endif
--- src/cvar.h
+++ src/cvar.h
@@ -1,6 +1,18 @@
 #ifndef OPENPREC_CVAR_H
 #define OPENPREC_CVAR_H
 
+#include "colordef.h"
+
+#define NOTIFY_COLOR    DEEP_ORANGE
+#define MARK_COLOR      DEEP_ORANGE
+
+// Use Msg/ColorMsg for raw prints
+#define ColorMsg(...) g_pCVar->ConsoleColorPrintf(__VA_ARGS__)
+#define ConNotifyf(X, ...) g_pCVar->ConsoleColorPrintf(NOTIFY_COLOR, "[%s]: " X "\n", OPENPREC_NAME, __VA_ARGS__)
+#define ConNotify(X) ConNotifyf("%s", X)
+#define ConMarkf(X, ...) g_pCVar->ConsoleColorPrintf(MARK_COLOR, "[%s]: " X "\n", OPENPREC_NAME, __VA_ARGS__)
+#define ConMark(X) ConMarkf("%s", X)
+
 bool register_cvars();
 void unregister_cvars();
 
